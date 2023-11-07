019afefb103bb4047a4afdf15453611babe73b16
luan-gabriel-brusarosco <luangabrielbrusarosco@gmail.com>
2023-11-07 08:53:02 -0300

    07-11-23

--- components/cadastrar/index.jsx
+++ components/cadastrar/index.jsx
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-    import { StyleSheet, Text, View } from 'react-native';
+    import { StyleSheet, Text, View, SafeAreaView, TextInput, Pressable, Alert } from 'react-native';
+
+    import Parse from 'parse/react-native.js';
+    import AnsyncStorage from '@react-native-async-storage/async-storage';
+
+    Parse.setAsyncStorage(AnsyncStorage);
+    const Api_ID = 'MS4tZJ76c9U9nrVG5nUArxtXR3AFTXp9gjvddXdq';
+    const JS_Key = 'JOcAMXYD49wALIHUsqEP9CQhl2cpX7TBEbu6sa6u';
+    Parse.initialize(Api_ID,JS_Key);
+    Parse.serverURL = 'https://parseapi.back4app.com/';
+
+    import 'react-native-get-random-values';
 
     export default function Cadastrar() {
     return (
+        <SafeAreaView style={styles.container__safe}>
         <View style={styles.container}>
-        <Text>Cadastrar</Text>
-        <StatusBar style="auto" />
+          <TextInput
+          placeholder={'Produto'}
+          style={style.input}
+          />
         </View>
+    </SafeAreaView>
     );
     }
 
     const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+        container__safe:{
+            flex: 1
+          },
+          container: {
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+          } 
     });
